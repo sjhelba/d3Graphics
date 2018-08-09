d5c0bb265b3a2c45290e5a0c2cd26162308d267f
Sara Helba <29603156+sjhelba@users.noreply.github.com>
2018-08-09 11:29:59 -0400

    tester

--- app.js
+++ app.js
@@ -1,270 +1,335 @@
-'use strict';
+var percentFilled = 50;
 
-console.log('js running');
+var borderColor = "#2c3e50";
+var borderPadding = 3;
+var borderWidth = 0;
 
-// (function start () {
-//     // LESS RECTANGLES THAN DATA POINTS
-//     const dataArray = [5, 40, 50, 60];
-//     const width = 500;
-//     const height = 500;
+var gaugeFaceColor = "white";
+var gaugeColor1 = "#339933";
+var gaugeColor2 = "#cccc66";
+var gaugeColor3 = "#ff3333";
 
-//     const widthScale = d3.scale.linear()
-//         .domain([0, dataArray[dataArray.length - 1]])
-//         .range([0, width]);
+var tickCount = 30;
+var tickOutlineWidth = 0;
+var tickPadding = 0;
 
-//     const color = d3.scale.linear()
-//         .domain([0, dataArray[dataArray.length - 1]])
-//         .range(['red', 'blue']);
+var textCenterOffset = 5;
+var valueCenterOffset = 30;
 
-//     const axis = d3.svg.axis()
-//         .ticks(5)   //without this, default is 10
-//         .scale(widthScale);
+var textColor = "#7a7979";
+var fontFamily = "Tahoma";
+var fontSizeText = "18.0pt";
+var fontSizeValue = "11.0pt";
 
-//     const firstCanvas = d3.select("#app").append("svg")
-//         .attr('width', width)
-//         .attr('height', height)
-        
+var startEnd = 2.4;
 
-//     const barGraphGroup = firstCanvas.append('g')
-//         .attr('transform', 'translate(50, 20)')
+var colors = [];
+var data = [];
+var fills=[];
 
-//     const bars = barGraphGroup.selectAll('rect')
-//         .data(dataArray)
-//         .enter()
-//             .append('rect')
-//             .attr('width', d => widthScale(d))
-//             .attr('height', 50)
-//             .attr('fill', d => color(d))
-//             .attr('y', (d, i) => i * 100);
+function shadeColor(color, percent) {   
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
 
-//     const axisGroup = barGraphGroup.append('g')
-//         .attr('transform', `translate(0, ${dataArray.length * 100})`)
-//         .call(axis);
+var padAngle = (tickOutlineWidth === 0) ? 0 : tickPadding;
 
+var width = 225 ,
+    height = 225,
+    radius = Math.min(width, height) / 2 - 10,
+    gaugeWidth = radius * 0.2;// Math.PI / (2*radius);
 
+var filled = tickCount * (percentFilled / 100);
 
-//     // MORE CIRCLES THAN DATA POINTS
-//     const data = [10];
+var borderStop = Math.ceil(100 - (100 * (borderWidth / radius))) + "%";
+console.log(borderStop);
 
-//     const secondCanvas = d3.select('#app').append('svg')
-//         .attr('width', 500)
-//         .attr('height', 500)
+//padAngle = Math.max(0.025, padAngle);
+//padAngle = Math.min(0.035, padAngle);
 
-//     const circle1 = secondCanvas.append('circle')
-//         .attr('cx', 50)
-//         .attr('cy', 100)
-//         .attr('r', 25)
-//         .attr('stroke', "purple")
+var outerRadius = radius - borderWidth - borderPadding;
+var innerRadius = outerRadius - gaugeWidth;
 
-
-//     const circle2 = secondCanvas.append('circle')
-//         .attr('cx', 50)
-//         .attr('cy', 200)
-//         .attr('r', 25)
-//         .attr('stroke', "yellow")
-
-//     const circles = secondCanvas.selectAll('circle')
-//         .data(data)
-//         .attr('fill', 'green')  // if num of circles === num of data points, just adding attr here would update them all, no exit or enter or anything req
-//         .exit()
-//             .attr('fill', 'blue')
-
-
-//     // ANIMATIONS
-
-//     const thirdCanvas = d3.select('#app').append('svg')
-//         .attr('width', 500)
-//         .attr('height', 500)
-
-//     const circle3 = thirdCanvas.append('circle')
-//         .attr('cx', 50)
-//         .attr('cy', 50)
-//         .attr('r', 25);
-
-//     circle3.transition()
-//         .duration(1500) //default is 500 (in m-secs)
-//         .delay(2000)    //wait two seconds before animating
-//         .attr('cx', 150)
-//         .transition()
-//             .attr('cy', 200)
-//             .attr('fill', 'green')
-//         .transition()
-//             .attr('cx', 50)
-//         .transition()
-//             .attr('cy', 50)
-//             .each('start', function () {d3.select(this).attr('fill', 'blue')})
-//             .each('end', function () {d3.select(this).attr('fill', 'red')})
+for (i=0; i<tickCount; i++){  
+  data.push(10)
+  if (i >= filled) {
+     if (i <= (tickCount * 0.33)) {
+        fills.push(shadeColor(gaugeColor1, 0.75));
+        colors.push(shadeColor(gaugeColor1, -0.25)); //colors.push('#ccffcc');
+      }
+    else {
+      if ((i > (tickCount * 0.33)) && (i <= (tickCount * 0.66))) {
+        fills.push(shadeColor(gaugeColor2, 0.75));
+        colors.push(shadeColor(gaugeColor2, -0.25)); //colors.push('#ffff99');
+      }
+      else {
+        fills.push(shadeColor(gaugeColor3, 0.75));
+        colors.push(shadeColor(gaugeColor3, -0.25));//colors.push('#ff9999');
+      }
+    }
+  }
+  else
+    if (i <= (tickCount * 0.33)) {
+        fills.push(gaugeColor1);
+        colors.push(shadeColor(gaugeColor1, 0.25));//colors.push('#339933');
+      }
+    else {
+      if ((i > (tickCount * 0.33)) && (i <= (tickCount * 0.66))) {
+        fills.push(gaugeColor2);
+        colors.push(shadeColor(gaugeColor2, 0.25));//colors.push('#ffff00');
+      }
+      else {
+        fills.push(gaugeColor3);
+        colors.push(shadeColor(gaugeColor3, 0.25));//colors.push('#cc0000');
+      }
+    }
+  }
 
 
-//     // d3's ARRAY METHODS
+var pie = d3.layout.pie()
+    .value(function(d) { return d; })
+    .sort(null)
+    .startAngle(-startEnd)
+    .endAngle(startEnd)
+    .padAngle(padAngle);
 
-//     const dataArr = [10, 20, 30, 40, 50];
-
-//     dataArr.sort(d3.descending);    // d3 has sorting funcs to pass in to native js sort()
-//     d3.min(dataArr);    // returns min val in arr
-//     d3.max(dataArr);    // returns max val in arr
-//     d3.extent(dataArr); // returns [minVal, maxVal] of arr
-//     d3.sum(dataArr);    // returns sum of arr elements (150)
-//     d3.mean(dataArr);   // returns 30
-//     d3.median(dataArr); // returns 30
-//     //etc.
-//     d3.shuffle(dataArr) // returns array of elements in random order
-
-//     // d3 USING EXTERNAL DATA
-
-//     // d3.csv()    // can be used same way as json below
-//     d3.json("myDataFile.json", function (error, data) {
-//         if (error) throw error;
-//         const canvas = d3.select('#app').append('svg')
-//             .attr('width', 500)
-//             .attr('height', 500)
-
-//         canvas.selectAll('rect')
-//             .data(data) // references data arg
-//             .enter()
-//                 .append('rect')
-//                 .attr('width', d => d.age * 10)
-//                 .attr('height', 46)
-//                 .attr('y', (d, i) => i * 50)
-//                 .attr('fill', 'green')
-
-//         canvas.selectAll('text')
-//             .data(data)
-//             .enter()
-//                 .append('text')
-//                     .attr('fill', 'white')
-//                     .attr('y', (d, i) => i * 50 + 23)
-//                     .text(d => d.name)
-//     })
-// })()
-
-// //PATHS
-
-// (function pathsSection () {
-//     const canvas = d3.select('#app').append('svg')
-//         .attr('width', 500)
-//         .attr('height', 500)
-
-//     const data = [
-//         {x: 10, y: 160},
-//         {x: 30, y: 40},
-//         {x: 150, y: 10}
-//     ]
-
-//     const group = canvas.append('g')
-
-//     const line = d3.svg.line()
-//         .x(d => d.x)    //each x axis
-//         .y(d => d.y)    //each y axis
-
-//     group.selectAll('path')
-//         .data([data]) //stores array within array so that all of the data is being passed in as just one element in the array
-//         .enter()
-//             .append('path')
-//                 .attr('d', line)
-//                 .attr('fill', 'none')
-//                 .attr('stroke', '#000')
-//                 .attr('stroke-width', 10)
+var arc = d3.svg.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
 
 
-// })()
-
-// // ARCS AND PIE/DONUT CHART
-
-// (function arcs () {
-//     const canvas = d3.select('#app').append('svg')
-//         .attr('width', 500)
-//         .attr('height', 500)
-    
-//     const group = canvas.append('g')
-//         .attr('transform', 'translate(100, 100)')
-    
-//     const r = 100;
-//     const p = Math.PI * 2
-
-//     const arc = d3.svg.arc()
-//         .innerRadius(r - 20)    //center to inside of donut
-//         .outerRadius(r)    //center to outside of donut
-//         .startAngle(0)     //angle at which arc starts (measured in radians)
-//         .endAngle(p - 1)     //angle at which arc ends (pi * 2 radians === a full circle)
-
-//     group.append('path')
-//         .attr('d', arc)
+var parent = d3.select("#app").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
 
+var radialGradient = parent.append("defs")
+  .append("radialGradient")
+    .attr("id", "radial-gradient");
 
-// })()
+radialGradient.append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", gaugeFaceColor);
 
+radialGradient.append("stop")
+    .attr("offset", borderStop)
+    .attr("stop-color", gaugeFaceColor);
 
-// // DONUT CHART
-// const data = [10, 50, 80]
-// const r = 300
-// const color = d3.scale.ordinal()
-//     .range(['red', 'blue', 'orange'])
+radialGradient.append("stop")
+    .attr("offset", borderStop)
+    .attr("stop-color", borderColor);
 
-// const canvas = d3.select('#app').append('svg')
-//     .attr('width', 1500)
-//     .attr('height', 1500)
+var finalColor = (borderStop === "100%") ? gaugeFaceColor : borderColor;
 
-// const group = canvas.append('g')
-//     .attr('transform', 'translate(300, 300)')
+radialGradient.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", finalColor);
 
-// const arc = d3.svg.arc()        //arc path generator
-//     .innerRadius(200)
-//     .outerRadius(r)
+var circle = parent.append("circle")
+    .attr("id", "circle")
+    .attr("cx", width/2)
+    .attr("cy", height/2)
+    .attr("r", radius)
+    .attr('stroke', '#444444')
+    .attr('stroke-width', 8)
+    .attr('padding', 10)
+    .attr("fill", "url(#radial-gradient)");
 
-// const pie = d3.layout.pie()
-//     .value(d => d)
-
-// const arcs = group.selectAll('.arc')
-//     .data(pie(data))
-//     .enter()
-//         .append('g')
-//             .attr('class', 'arc')
-
-// arcs.append('path')
-//     .attr('d', arc)
-//     .attr('fill', d => color(d.data))
-
-// arcs.append('text')
-//     .attr('transform', d => `translate(${arc.centroid(d)})`)
-//     .attr('text-anchor', 'middle')
-//     .attr('font-size', '1.5em')
-//     .text(d => d.data)
+var svg = parent.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 
-// PIE CHART
-const data = [10, 50, 80]
-const r = 300
-const color = d3.scale.ordinal()
-    .range(['red', 'blue', 'orange'])
+var line1 = parent.append("text")
+    .attr("text-anchor", "middle")
+    .style('font-family', fontFamily)
+    .style('font-size', fontSizeText)
+    .attr("fill", textColor)
+    .text('System');
 
-const canvas = d3.select('#app').append('svg')
-    .attr('width', 1500)
-    .attr('height', 1500)
+var line2 = parent.append("text")
+    .attr("text-anchor", "middle")
+    .style('font-family', fontFamily)
+    .style('font-size', fontSizeText)
+    .attr("fill", textColor)
+    .text('Efficiency');
 
-const group = canvas.append('g')
-    .attr('transform', 'translate(300, 300)')
+var tmpVal = (percentFilled / 100).toFixed(2);
 
-const arc = d3.svg.arc()        //arc path generator
-    .innerRadius(0) //ONLY CHANGE FROM DONUT CHART
-    .outerRadius(r)
+var value = parent.append("text")
+    .style('font-family', fontFamily)
+    .style('font-size', fontSizeValue)
+    .style('font-weight', 'bold')
+    .attr("fill", 'black')
+    .attr("text-anchor", "middle")
+    .text(tmpVal + ' kW/tR');
 
-const pie = d3.layout.pie()
-    .value(d => d)
+var th = line1.node().getBBox().height;
 
-const arcs = group.selectAll('.arc')
-    .data(pie(data))
-    .enter()
-        .append('g')
-            .attr('class', 'arc')
+line1.attr("transform", "translate(" + width / 2 + "," + ((height / 2.25) - (th / 2) + textCenterOffset) + ")");
+line2.attr("transform", "translate(" + width / 2 + "," + ((height / 2.25) + (th / 2) + textCenterOffset) + ")");
+value.attr("transform", "translate(" + width / 2 + "," + ((height / 2) + (th / 1.5) + valueCenterOffset) + ")");
 
-arcs.append('path')
-    .attr('d', arc)
-    .attr('fill', d => color(d.data))
+// d3.tsv("data.tsv", type, function(error, data) {
+ var actStroke = (tickOutlineWidth === 0) ? 1 : tickOutlineWidth;
 
-arcs.append('text')
-    .attr('transform', d => `translate(${arc.centroid(d)})`)
-    .attr('text-anchor', 'middle')
-    .attr('font-size', '1.5em')
-    .text(d => d.data)
+ var path = svg.datum(data).selectAll("path")
+  .data(pie(data));
+
+path.enter().append("path")
+    //   .data(pie)
+    // .enter().append("path")
+      .attr("stroke", function(d, i) { 
+            if (tickOutlineWidth === 0)
+              {return fills[i]}
+            else {
+                return colors[i]; 
+            }
+            })
+      .attr("stroke-width", actStroke)
+      .attr("fill", function(d, i) { return fills[i]; })
+      .attr("d", arc); // store the initial angles
+
+path.exit().remove();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use strict';
+
+// console.log('js running');
+
+// const tR = 3000
+// const kW = 1625
+// const kWPerTR = kW / tR
+// // .06 kW/tR each
+// // 30 bars * .06 = 1.8
+// // gauge goes from 0 to 1.8.
+
+// // const canvas = d3.select('#app').append('svg')
+// //     .attr('width', 500)
+// //     .attr('height', 500)
+
+// // const group = canvas.append('g')
+// //     .attr('transform', 'translate(100, 100)')
+
+// // const r = 100;
+// // const p = Math.PI * 2
+
+// // const arc = d3.svg.arc()
+// //     .innerRadius(r - 30)    //center to inside of donut
+// //     .outerRadius(r)    //center to outside of donut
+// //     .startAngle(p - 2.45)     //angle at which arc starts (measured in radians)
+// //     .endAngle(p + 2.45)     //angle at which arc ends (pi * 2 radians === a full circle)
+
+// // group.append('path')
+// //     .attr('d', arc)
+
+
+
+
+
+//     // EXAMPLE
+
+
+
+//     var width = 960,
+//     height = 500,
+//     twoPi = 2 * Math.PI,
+//     progress = 0,
+//     total = 1308573, // must be hard-coded if server doesn't report Content-Length
+//     formatUnit = d3.format(".00");
+
+// var arc = d3.svg.arc()
+//     .startAngle(0)
+//     .innerRadius(180)
+//     .outerRadius(240);
+
+// var svg = d3.select("#app").append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+//   .append("g")
+//     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+// var meter = svg.append("g")
+//     .attr("class", "progress-meter");
+
+// meter.append("path")
+//     .attr("class", "background")
+//     .attr("d", arc.endAngle(twoPi));
+
+// var foreground = meter.append("path")
+//     .attr("class", "foreground");
+
+// var text = meter.append("text")
+//     .attr("text-anchor", "middle")
+//     .attr("dy", ".35em");
+
+// var i = d3.interpolateNumber(kWPerTR, 1.8)
+// var efficiency = i(kWPerTR)
+// foreground.attr('d', arc.endAngle(twoPi * efficiency))
+// text.text(kWPerTR)
+
+// // d3.transition()
+// //     .tween('units', () => () => {})
+
+// // d3.json("https://api.github.com/repos/mbostock/d3/git/blobs/2e0e3b6305fa10c1a89d1dfd6478b1fe7bc19c1e?" + Math.random())
+// //     .on("progress", function() {
+// //       var i = d3.interpolate(progress, d3.event.loaded / total);
+// //       d3.transition().tween("progress", function() {
+// //         return function(t) {
+// //           progress = i(t);
+// //           foreground.attr("d", arc.endAngle(twoPi * progress));
+// //           text.text(formatPercent(progress));
+// //         };
+// //       });
+// //     })
+// //     .get(function(error, data) {
+// //       meter.transition().delay(250).attr("transform", "scale(0)");
+// //     });
